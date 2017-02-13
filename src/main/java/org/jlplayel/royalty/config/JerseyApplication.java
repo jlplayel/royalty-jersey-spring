2b5f968240b0ef869b9f6e7b2a20e74726ded5d6
jlplayel <thonolan5@gmail.com>
2017-02-13 14:16:45 -0800

    0007: Fixing the package name in the configuration.

--- src/main/java/org/jlplayel/royalty/config/JerseyApplication.java
+++ src/main/java/org/jlplayel/royalty/config/JerseyApplication.java
@@ -4,6 +4,6 @@ import org.glassfish.jersey.server.ResourceConfig;
 
 public class JerseyApplication extends ResourceConfig {
     public JerseyApplication() {
-        packages("org.jlplayel.royalty.controller;org.jlplayel.royalty.errorHandler");
+        packages("org.jlplayel.royalty.controller;org.jlplayel.royalty.errorhandler");
     }
 }
